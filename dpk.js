0dd6294c4713e14f850da4ebfb4cf8b6c26487c8
humbertoarizahsl <102244335+humbertoarizahsl@users.noreply.github.com>
2022-12-14 01:05:18 -0500

    finished the refactoring of deterministicPartitionKey function

--- dpk.js
+++ dpk.js
@@ -1,29 +1,27 @@
 const crypto = require("crypto");
 
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+const createHash = (method, data) => (crypto.createHash(method).update(data).digest("hex"))
+
+const handleHash = (method, data) => {
+  if (data.length > MAX_PARTITION_KEY_LENGTH) return createHash(method, data)
+  return data
+}
+
+const validateTypeCandidate = (candidate) => (typeof candidate !== "string" ? JSON.stringify(candidate): candidate)
+
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate
+  let candidate = TRIVIAL_PARTITION_KEY
 
   if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+    if (!event.partitionKey) return createHash("sha3-512", JSON.stringify(event))
+    candidate = event.partitionKey
   }
+  candidate = validateTypeCandidate(candidate)
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  console.log(candidate)
+  candidate = handleHash("sha3-512",candidate)
+
   return candidate;
 };
--- dpk.test.js
+++ dpk.test.js
@@ -1,14 +1,17 @@
 const { deterministicPartitionKey } = require("./dpk");
 describe("deterministicPartitionKey", () => {
-  const mockPartitionKey = {}
 
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
-  it("Returns a hash when the event contain a partition key with lenght < 256", () => {
-    const trivialKey = deterministicPartitionKey({partitionKey: '243241243'});
-    expect(trivialKey).toBe("243241243");
+  it("Returns a hash when there is an event but does not contain the property partitionKey", () => {
+    const trivialKey = deterministicPartitionKey({test:'0'});
+    expect(trivialKey).toBe("2fc7ecbc698107c564fdf9463f0c72bc32012e66b0200a5dfb6dd8f5c9d684a53816cd91425ba7d9021b925639138d1adfe99bdd6b47a265944bd815d818e54a");
+  });
+  it("Returns the literal when the event contain a partition key with lenght < 256", () => {
+    const trivialKey = deterministicPartitionKey({partitionKey: '123456789'});
+    expect(trivialKey).toBe("123456789");
   });
   it("Returns a hash when the event contain a partition key with lenght > 256", () => {
     const trivialKey = deterministicPartitionKey({partitionKey:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."});
